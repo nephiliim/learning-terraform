2b42117a04f5c656377bc52d1adace83ffc52ac7
nephiliim <nephiliim@users.noreply.github.com>
2024-04-15 17:26:56 +0100

    Update main.tf
    group

--- main.tf
+++ main.tf
@@ -14,11 +14,54 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.nano"
 
+vpn_security_group_ids = [aws_security_group.blog.id]
+
   tags = {
     Name = "HelloWorld"
   }
+}
+
+
+resource "aws_security_group" "blog" {
+  name = "blog"
+  description = "Allow http and https in. Allow everything out"
+
+  vpn_id = data.aws_vpc.defult.id
+}
+
+resource "aws_security_group_rule " "blog_http_in" {
+  type        = "ingress" 
+  from_port   = 80
+  to_port     = 80
+  protocol    = "TCP"
+  cidr_blocks = ["0.0.0/0"]
+
+  secuity_group_id -= aws_secuirty_group.blog.id
+}
+
+resource "aws_security_group_rule " "blog_https_in" {
+  type        = "ingress" 
+  from_port   = 443
+  to_port     = 443
+  protocol    = "TCP"
+  cidr_blocks = ["0.0.0/0"]
+
+  secuity_group_id -= aws_secuirty_group.blog.id
+}
+
+resource "aws_security_group_rule " "blog_everything_out" {
+  type        = "engress" 
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0/0"]
+
+  secuity_group_id -= aws_secuirty_group.blog.id
 }
