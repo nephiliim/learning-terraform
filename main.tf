3d1d357e32bf631d7af53c36043738ed1b6b918f
nephiliim <nephiliim@users.noreply.github.com>
2024-04-15 21:52:04 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,10 +37,10 @@ module "Blog_sg" {
 vpc_id              = data.aws_vpc.default.id
 
 ingress_rules       = ["http-80-tcp","https-443-tcp"]
-ingress_cide_blocks = ["0.0.0.0/0"]
+ingress_cidr_blocks = ["0.0.0.0/0"]
 
 egress_rules       = ["all-all"]
-egress_cide_blocks = ["0.0.0.0/0"]
+egress_cidr_blocks = ["0.0.0.0/0"]
 
 }
 
