85a87c555dc6254c366faa4338193f566d7294a6
nephiliim <nephiliim@users.noreply.github.com>
2024-04-15 23:33:17 +0100

    blog to Blog

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ module "autoscaling" {
   max_size = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog.target_group_arns
+  target_group_arns   = module.Blog.target_group_arns
   security_groups = [module.Blog_sg.security_group_id]
   
   image_id                 = data.aws_ami.app_ami.id
